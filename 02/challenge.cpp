c1d2c2b34cdd26a2162359648c9fd2fa0ba31631
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-12-03 00:46:48 +0100

    Day 2 Task 2

--- 02/challenge.cpp
+++ 02/challenge.cpp
@@ -45,6 +45,29 @@ bool isMonotonic(std::ranges::input_range auto&& digits) {
     });
 }
 
+bool isStrictMonotonic(int a, int b, int c){
+    if (a < b && b < c) return true;
+    if (a > b && b > c) return true;
+    return false;
+}
+
+bool isFlat(int a, int b){
+    int difference = std::abs(a - b);
+    return difference >= 1 && difference <= 3;
+}
+
+bool isFlat(int a, int b, int c){
+    return isFlat(a, b) && isFlat(b, c);
+}
+
+bool isGood(int a, int b, int c) {
+    if(not isFlat(a, b, c)) return false;
+    if(a == b || b == c) return false;
+    if(a < b && b > c) return false;
+    if(a > b && b < c) return false;
+    return true;
+}
+
 int main(int argc, char* argv[]){
     if (argc != 2){
         return EXIT_FAILURE;
@@ -64,7 +87,6 @@ int main(int argc, char* argv[]){
             return std::string_view{e};
           });
     for(auto const line : lines){
-        // std::cout << "Line: " << line << std::endl;
         if(line.size() == 0) continue;
 
         auto digits = line 
@@ -76,26 +98,129 @@ int main(int argc, char* argv[]){
                 return std::strtol(e.data(), nullptr, 10);
             });
 
-        auto differences = digits 
-            | std::views::adjacent<2>
-            | std::views::transform([](auto const& pair){
-                auto const [left, right] = pair; // TODO transform to spread tuple paramters
-                // TODO transorm difference, transform sign, transform gradientt
-                return right - left;
-            })
-            | std::views::transform([](auto const& e){
-                return std::abs(e);
-            });
-        auto const flat = std::ranges::all_of(differences, [](auto const& e){
-            return e >= 1 && e <= 3;
-        });
+        // auto differences = digits 
+        //     | std::views::adjacent<2>
+        //     | std::views::transform([](auto const& pair){
+        //         auto const [left, right] = pair; // TODO transform to spread tuple paramters
+        //         // TODO transorm difference, transform sign, transform gradientt
+        //         return right - left;
+        //     })
+        //     | std::views::transform([](auto const& e){
+        //         return std::abs(e);
+        //     });
+        // auto const flat = std::ranges::all_of(differences, [](auto const& e){
+        //     return e >= 1 && e <= 3;
+        // });
 
-        if(flat && isMonotonic(digits)){
-            result++;
+        // if(flat && isMonotonic(digits)){
+        //     result++;
+        // }
+
+
+        auto windows = digits 
+            | std::views::adjacent<3>
+            | std::views::transform([](auto const& triple){
+                auto [a, b, c] = triple;
+                // std::cout << "Window: " << a << "," << b << "," << c << std::endl;
+                // std::cout << "strict monotonic: " << isStrictMonotonic(a, b, c) << std::endl; 
+                // std::cout << "flat: " << isFlat(a, b, c) << std::endl; 
+                
+                bool good = isFlat(a, b, c) && isStrictMonotonic(a, b, c);
+
+                return not good;
+            });
+
+        auto numBad = std::ranges::count_if(windows, std::identity{});
+        if(numBad > 3) continue;
+        if(numBad == 0){
+            result += 1;
+            continue;
         }
+
+        std::size_t firstBad = std::distance(windows.begin(), std::ranges::find(windows, true));
+        auto x = std::ranges::find_last(windows, true);
+        std::size_t lastBad = std::distance(windows.begin(), x.begin());
+        
+                //  check that all errors are one contigous window
+        auto distance = lastBad - firstBad + 1;
+        auto badWindowsContigous = distance == numBad;
+
+        if(not badWindowsContigous) continue;
+
+        // if the outlier is in the middle, than we need to be flat over outer values
+        auto it = digits.begin();
+        for(std::size_t i = 0; i < firstBad; i++) it++;
+        auto a = *it; it++;
+        auto b = *it; it++;
+        auto c = *it; it++;
+        auto d = *it; it++;
+        auto e = *it; it++;
+
+        // auto const flatAroundMiddleOutlier = isFlat(b, d);
+        // if(not flatAroundMiddleOutlier) continue;
+
+        // if there is excatly one error, it is eithehr
+        // at the beginning or end, which is fine, because we can simply delete the first or alst
+        // or if it is in the midde it is a saddle point which is not fine
+        if(numBad == 1){
+            if (firstBad == 0){
+                // beginning, fine
+                result++;
+                continue;
+            } else if (firstBad == std::distance(windows.begin(), windows.end()) - 1){
+                // last, fine
+                result++;
+                continue;
+            } else {
+                // saddle point, bad
+                continue;
+            }
+        }
+
+        if(numBad == 3) continue;
+
+        if(numBad == 2){
+            if(a == d){
+                // also a saddle point
+                continue;
+            }
+
+            if( 
+                // removing one of the bad candidates should make this flat...
+                isGood(a, c, d) 
+                || isGood(a, b, d) 
+            ) {
+                result++;
+                continue;
+            }
+
+            // // all remaining look good
+            // result++;
+            // continue;
+        }
+
+
+
+
+        std::cout << "Line: " << line << std::endl;
+        std::cout << "          ";
+        for(auto x : windows) {
+            std::cout << x << "  ";
+        }
+        std::cout << std::endl;
+
+
+        std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
+        std::cout << firstBad << "/" << lastBad << std::endl;
+
+
+
+        // result += 1;
     }
 
     std::cout << result << std::endl;
 
     return EXIT_SUCCESS;
 }
+
+// between 360 and 388
