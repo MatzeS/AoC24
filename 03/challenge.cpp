b3e7325659eb1528be0f1d183110d91568182c1b
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-12-04 00:27:39 +0100

    Day 3 Part 2

--- 03/challenge.cpp
+++ 03/challenge.cpp
@@ -23,23 +23,32 @@ int main(int argc, char* argv[]){
 
     std::string inputString = input.str();
 
+    auto const enable = std::string_view{"do()"};
+    auto const disable = std::string_view{"don't()"};
 
-    std::regex expression("mul\\((\\d*),(\\d*)\\)");
+    std::regex expression(R"~(mul\((\d*),(\d*)\)|do\(\)|don't\(\))~");
     auto matchesBegin = std::sregex_iterator(inputString.begin(), inputString.end(), expression);
     auto matchesEnd = std::sregex_iterator();
  
     
     uint32_t sum = 0;
+    auto enabled = true;
     for (std::sregex_iterator it = matchesBegin; it != matchesEnd; it++){
         std::smatch match = *it;
 
-        auto groupIterator = match.begin();
-        groupIterator++;
-        auto const firstNumber = std::stoi(groupIterator->str()); 
-        groupIterator++;
-        auto const secondNumber = std::stoi(groupIterator->str()); 
-        
-        sum += firstNumber * secondNumber;
+        if(match.str() == enable){
+            enabled = true;
+        } else if(match.str() == disable){
+            enabled = false;
+        } else if(enabled) {
+            auto groupIterator = match.begin();
+            groupIterator++;
+            auto const firstNumber = std::stoi(groupIterator->str()); 
+            groupIterator++;
+            auto const secondNumber = std::stoi(groupIterator->str()); 
+            
+            sum += firstNumber * secondNumber;
+        }
     }
 
     std::cout << sum << std::endl;
