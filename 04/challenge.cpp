2918ee187cd5de2c52c16b81b2dcc900695fe6fc
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2024-12-04 21:32:52 +0100

    Day 4 Part 2

--- 04/challenge.cpp
+++ 04/challenge.cpp
@@ -44,51 +44,53 @@ int main(int argc, char* argv[]){
     }
 
 
+    std::array stencils{
+        std::array{
+            std::array{'M', '.', 'M'},
+            std::array{'.', 'A', '.'},
+            std::array{'S', '.', 'S'}
+        },
+        std::array{
+            std::array{'S', '.', 'M'},
+            std::array{'.', 'A', '.'},
+            std::array{'S', '.', 'M'}
+        },
+        std::array{
+            std::array{'M', '.', 'S'},
+            std::array{'.', 'A', '.'},
+            std::array{'M', '.', 'S'}
+        },
+        std::array{
+            std::array{'S', '.', 'S'},
+            std::array{'.', 'A', '.'},
+            std::array{'M', '.', 'M'}
+        }
+    };
+
+
+
     uint32_t sum = 0;
-    for(uint32_t y = 0; y < data.size(); y++){
+    for(uint32_t y = 0; y < data.size() - 2; y++){
         auto const length = data[y].size();
-        for(uint32_t x = 0; x < length; x++){
+        for(uint32_t x = 0; x < length - 2; x++){
 
-            std::array<std::array<int, 2>, 8> directions{
-                std::array<int, 2>{ 0,  1},
-                std::array<int, 2>{ 0, -1},
-                std::array<int, 2>{ 1,  0},
-                std::array<int, 2>{-1,  0},
-                std::array<int, 2>{ 1,  1},
-                std::array<int, 2>{-1,  1},
-                std::array<int, 2>{ 1, -1},
-                std::array<int, 2>{-1, -1}
-            };
+            for(auto stencil : stencils){
 
-            auto const word = std::string_view{"XMAS"};
-            assert(word.size() == 4);
-
-            for(auto const direction : directions){
-                int32_t px = x;
-                int32_t py = y;
-
-                // std::cout << "new" << y << "/" << x << std::endl;
-                for(uint8_t i = 0; i < word.size(); i++){
-                    if(word[i] != data.at(py).at(px)){ 
-                        // std::cout << word[i] << "/" << data[y][x] << "  " << py << "/" << px << std::endl;
-                        break;
-                    }
+                for(uint32_t sy = 0; sy < 3; sy++){
+                    for(uint32_t sx = 0; sx < 3; sx++){
+                        if(stencil[sy][sx] == '.') continue;
+                        if(stencil[sy][sx] != data[y+sy][x+sx]){
+                            sx = 4;
+                            sy = 4;
+                            break;
+                        }
                     
-                    if(i == word.size() - 1){
-                        sum++;
-                        break;
+                        if(sy == 2 && sx == 2){
+                            sum++;
+                        }
                     }
-
-                    py += direction[0];
-                    px += direction[1];
-
-
-                    if(py < 0) break;
-                    if(px < 0) break;
-                    if(py == data.size()) break;
-                    if(px == data[y].size()) break;
-
                 }
+
             }
 
         }
